3ac07c89e1693954b6a805fa6ea85286c53ff548
yougong0215 <yougong0215@gmail.com>
2022-04-16 20:00:54 +0900

    스프라이트 교정함

--- Assets/Script/Player/AttackR.cs
+++ Assets/Script/Player/AttackR.cs
@@ -5,15 +5,12 @@ using UnityEngine.UI;
 
 public class AttackR : MonoBehaviour
 {
-    public GameObject playerSpriteRenderer;
-    // Start is called before the first frame update
     void Start()
     {
         
         transform.localScale = new Vector3(0.3f, 0.8f, 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
